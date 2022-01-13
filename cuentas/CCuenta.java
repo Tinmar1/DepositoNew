176c6b65c2226dc62495260a9d435e55d726af22
Tinmar1 <tinmartravian@hotmail.com>
2022-01-13 17:54:03 +0100

    Versión 2.0 Commit tras documentar las clases

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,17 +1,37 @@
 package cuentas;
 
+/**
+ * Esta clase simula una cuenta bancaria.
+ * 
+ * @author Martin
+ * @version 2.0
+ * @since 1.0
+ * 
+ */
 public class CCuenta {
 
-
+/**
+ * Atributos de la clase.
+ */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInter;
 
+/**
+ * Constructor sin parámetros.
+ */
     public CCuenta()
     {
     }
 
+/**
+ * Constructor con todos los parámetros.
+ * @param nom es el nombre del titular
+ * @param cue es el número de la cuenta
+ * @param sal es la cantidad de dinero que hay en la cuenta
+ * @param tipo es el tipo de interés de la cuenta
+ */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -19,11 +39,20 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+/**
+ * Método que devuelve el saldo de la cuenta.
+ * @return saldo
+ */
     public double estado()
     {
         return getSaldo();
     }
 
+/**
+ * Método para realizar un ingreso en la cuenta.
+ * @param cantidad es el dinero que se va a ingresar en la cuenta
+ * @throws Exception cuando se trata de ingresar una cantidad negativa
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +60,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+ /**
+  * Método para retirar una cantidad de la cuenta.
+  * @param cantidad es el dinero que se quiere retirar
+  * @throws Exception cuando la cantidad sea negativa
+  * @throws Exception cuando la cantidad sea mayor de el dinero que hay en la cuenta
+  */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -40,34 +75,66 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+/**
+ * Método para obtener el nombre
+ * @return nombre
+ */
 	private String getNombre() {
 		return nombre;
 	}
 
+/**
+ * Método para asignar una cadena de texto al nombre
+ * @param nombre String con el titular de la cuenta
+ */
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+/**
+* Método para obtener el número de cuenta
+* @return cuenta
+*/
 	private String getCuenta() {
 		return cuenta;
 	}
 
+/**
+ * Método para asignar una cadena de texto a cuenta
+ * @param cuenta string con el número de cuenta
+ */
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+/**
+* Método para obtener el saldo
+* @return saldo
+*/
 	private double getSaldo() {
 		return saldo;
 	}
 
+/**
+ * Método para asignar un valor al saldo
+ * @param saldo valor de la cantidad de dinero en la cuenta
+ */
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+/**
+* Método para obtener el tipo de interés
+* @return tipoInter
+*/
 	private double getTipoInter() {
 		return tipoInter;
 	}
 
+/**
+ * Método para asignar un valor al tipoInter
+ * @param tipoInter valor del tipo de interés de la cuenta
+ */
 	private void setTipoInter(double tipoInter) {
 		this.tipoInter = tipoInter;
 	}
--- cuentas/Main.java
+++ cuentas/Main.java
@@ -1,18 +1,33 @@
 package cuentas;
 
+/**
+ * Clase para arrancar el programa.
+ * 
+ * @author Martin
+ * @version 2.0
+ * @since 1.0
+ */
 public class Main {
 
+/**
+ * Método main
+ * @param args argumentos
+ */
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
 
-        operativa_cuenta(0);
+        operativa_cuenta(2500);
     }
 
+ /**
+  * Método para realizar operaciones sobre un objeto CCuenta.
+  * @param cantidad valor del saldo con el que se crea la cuenta
+  */
 	private static void operativa_cuenta(float cantidad) {
 		CCuenta cuenta1;
 		double saldoActual;		
-		cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
+		cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",cantidad,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
